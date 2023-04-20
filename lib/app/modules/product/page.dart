c5defebab9d1187e3d54abc7f9740f0b279801c7
Fernando Gnadt de Oliveira <fernando.oliveira@mobilus.com.br>
2023-04-20 18:32:38 -0300

    produtos

---
+++ lib/app/modules/product/page.dart
@@ -0,0 +1,12 @@
+import 'package:app_hortifruti_pratico/app/modules/product/controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ProdutoPage extends GetView<ProdutoController> {
+  const ProdutoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/app/modules/store/page.dart
+++ lib/app/modules/store/page.dart
@@ -1,7 +1,9 @@
 import 'package:app_hortifruti_pratico/app/modules/store/controller.dart';
+import 'package:app_hortifruti_pratico/app/routes/routes.dart';
 import 'package:app_hortifruti_pratico/app/widgets/store_status.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class StorePage extends GetView<StoreController> {
@@ -10,42 +12,101 @@ class StorePage extends GetView<StoreController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       body: controller.obx(
-        (state) => ListView(
-          children: [
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
-              child: Row(
-                children: [
-                  SizedBox(
-                    width: 96.0,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(8.0),
-                      child: FadeInImage.memoryNetwork(
-                          placeholder: kTransparentImage, image: state!.image),
+        (state) => CustomScrollView(
+          slivers: [
+            const SliverAppBar(),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(16, 8, 16, 16),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 96.0,
+                      height: 96.0,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: FadeInImage.memoryNetwork(
+                            placeholder: kTransparentImage,
+                            image: state!.image),
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 16.0),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          state.nome,
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: Get.textTheme.headlineSmall,
-                        ),
-                        const SizedBox(height: 8.0),
-                        StoreStatus(isOnline: state.isOnline),
-                      ],
+                    const SizedBox(width: 16.0),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            state.nome,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: Get.textTheme.headlineSmall,
+                          ),
+                          const SizedBox(height: 8.0),
+                          StoreStatus(isOnline: state.isOnline),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            )
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                childCount: state.categorias.length,
+                (context, index) {
+                  var categoria = state.categorias[index];
+                  return Column(
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 8, horizontal: 16),
+                              color: Colors.grey[200],
+                              child: Text(
+                                categoria.nome,
+                                style: Get.textTheme.titleMedium!
+                                    .copyWith(fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      for (var produto in categoria.produtos)
+                        ListTile(
+                          title: Text(produto.nome),
+                          subtitle: Text(
+                            NumberFormat.simpleCurrency().format(produto.preco),
+                          ),
+                          leading: produto.imagem.isNotEmpty
+                              ? SizedBox(
+                                  width: 56.0,
+                                  height: 56.0,
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(8.0),
+                                    child: FadeInImage.memoryNetwork(
+                                      placeholder: kTransparentImage,
+                                      image:
+                                          'https://loremflickr.com/300/300/food',
+                                    ),
+                                  ),
+                                )
+                              : null,
+                          onTap: () => Get.toNamed(
+                            Routes.product,
+                            arguments: {
+                              'product': produto,
+                              'store': state,
+                            },
+                          ),
+                        ),
+                    ],
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
